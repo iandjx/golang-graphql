b842c71fc3b85e88fe52efd302fe33c576cdd5bc
Ian de Jesus <ian@eskwelabs.com>
2021-08-22 16:02:52 +0900

    initial commit

---
+++ readme.md
@@ -0,0 +1,6 @@
+# Hacker News GraphQL Backend
+
+## Instruction
+
+1. run docker-compose.yml
+2. go run server.go
---
+++ server.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/99designs/gqlgen/graphql/handler"
+
+	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/go-chi/chi"
+	"github.com/iandjx/hackernews/graph"
+	"github.com/iandjx/hackernews/graph/generated"
+	"github.com/iandjx/hackernews/internal/auth"
+	database "github.com/iandjx/hackernews/internal/pkg/db/mysql"
+)
+
+const defaultPort = "8080"
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = defaultPort
+	}
+
+	router := chi.NewRouter()
+	router.Use(auth.Middleware())
+
+	database.InitDB()
+	database.Migrate()
+	server := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
+	router.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	router.Handle("/query", server)
+
+	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
+	log.Fatal(http.ListenAndServe(":"+port, router))
+}
